04b953b59c4adaeac57280eb3d49deaa3febe4f9
daisiduke <jason-ti@web.de>
2023-02-24 12:13:05 +0100

    Update progression class

--- data_structures_and_algorithms_exercises/progressions.py
+++ data_structures_and_algorithms_exercises/progressions.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Progression:
 
     def __init__(self, start):
@@ -18,7 +21,7 @@ class Progression:
         return self
 
     def print_progression(self, n):
-        print(' '.join(str(next(self)) for j in range(n)))
+        print(' '.join(str(next(self)) for _ in range(n)))
 
 
 class FibonacciProgression(Progression):
@@ -31,5 +34,26 @@ class FibonacciProgression(Progression):
         self._previous, self._current = self._current, self._previous + self._current
 
 
-test = FibonacciProgression(2, 2)
+class AbsDifferenceProgression(Progression):
+
+    def __init__(self, first_number=2, second_number=200):
+        super().__init__(first_number)
+        self._previous = abs(first_number + second_number)
+
+    def _advance(self):
+        self._previous, self._current = self._current, abs(self._previous - self._current)
+
+
+class SquareRootProgression(Progression):
+
+    def __init__(self, first_number=65536):
+        super().__init__(first_number)
+
+    def _advance(self):
+        self._current = math.sqrt(self._current)
+
+
+# test = FibonacciProgression(2, 2)
+# test.print_progression(10)
+test = SquareRootProgression()
 test.print_progression(10)
